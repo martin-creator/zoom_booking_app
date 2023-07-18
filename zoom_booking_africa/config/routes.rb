30ad1b3fd65c9132e6d3db604700887a42cb4425
martin-creator <martinlubowa@outlook.com>
2023-07-18 12:39:47 +0300

    created home, contact, thank you, about pages

--- zoom_booking_africa/config/routes.rb
+++ zoom_booking_africa/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'dasboard', to: 'pages#dasboard'
+  get 'thank_you', to: 'pages#thank_you'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
