3c9a1b9e89f77ac842947a3ed56301ed912003a7
martin-creator <martinlubowa@outlook.com>
2023-07-22 10:04:24 +0300

    added logic to create zoom meetings

--- zoom_booking_africa/lib/zoom.rb
+++ zoom_booking_africa/lib/zoom.rb
@@ -9,25 +9,34 @@ module Zoom
     USER_ID = "aflSj9rETLSKD8FGU3Fu5g"
 
     def create_meeting(payload)
+      begin
+        access_token = get_access_token()
+        response = HTTP.post(
+          "https://api.zoom.us/v2/users/#{USER_ID}/meetings",
+          headers: { authorization: access_token },
+          json: payload
+        )
+      rescue Exception => e
+        puts e.message
+      end
     end
 
     def update_meeting(meeting_id, payload)
     end
 
     private
-    
+
     def get_access_token()
       url = "https://zoom.us/oauth/token"
       auth = Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")
       response = HTTP.post(url, :form => {
-        :grant_type => "account_credentials"
-      }, :headers => {
-        "Authorization" => "Basic #{auth}"
-      })
-      
+                                  :grant_type => "account_credentials",
+                                }, :headers => {
+                                  "Authorization" => "Basic #{auth}",
+                                })
+
       data = response.parse
       access_token = "Bearer #{data["access_token"]}"
     end
-
   end
 end
