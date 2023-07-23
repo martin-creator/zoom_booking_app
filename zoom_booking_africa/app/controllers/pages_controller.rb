ad0a96d6b74a7a09497325c50b77a549dd14899c
martin-creator <martinlubowa@outlook.com>
2023-07-23 10:59:27 +0300

    Added logic to convert receipt to pdf

--- zoom_booking_africa/app/controllers/pages_controller.rb
+++ zoom_booking_africa/app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:dashboard]
+  before_action :authenticate_user!, only: [:dashboard, :receipt]
 
   def home
     @meetings = Meeting.upcoming
@@ -16,6 +16,24 @@ class PagesController < ApplicationController
     @my_upcoming_meetings = Meeting.upcoming.joins(:bookings).where("bookings.user_id = ?", current_user.id)
 
     @my_bookings = current_user.bookings
+  end
+
+  def receipt
+    @booking = current_user.bookings.where(id: params[:booking_id]).first
+
+    if @booking.nil?
+      respond_to do |format|
+        format.html
+        format.pdf do
+          render pdf: "booking_#{@booking.id}", template: "pages/receipt", formats: [:html]  # Excluding ".pdf" extension.
+        end
+      end
+    else
+
+      redirect_to_dashboard
+      
+    end
+  
   end
 
   def thank_you
